83dc6dd1a2710d80c2248e7dd3908a2eeb05bfaa
Velinu <matheusfelintosilva2@gmail.com>
2023-07-05 09:32:39 -0300

    refactor: add Timer.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+  </header>
+  <main>
+    <TimerMain/>
+  </main>
+  <Footer>
+  </Footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimerMain from './components/Timer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TimerMain
+  },
+  data() {
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
 }
 </style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="clock">
+    <h1>{{ hours }}:</h1>
+    <h1>{{ minutes }}:</h1> 
+    <h1>{{ seconds }}</h1>
+</div>
+</template>
+
+<script>
+export default{
+    name: "TimerMain",
+    data() {
+        return{
+            hours: '00',
+            minutes: '00',
+            seconds: '00'
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style scoped>
+    #clock{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
