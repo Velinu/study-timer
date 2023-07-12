eb79ecb0ede62a826bcfc68c1a5c93fdb57ac62b
Velinu <matheusfelintosilva2@gmail.com>
2023-07-12 11:26:36 -0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/study-timer-build/'
+    ? '/study-timer/'
     : '/'
 })
