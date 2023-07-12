d063e8aae54638cadc9b782a6ac705e3e681f600
Velinu <matheusfelintosilva2@gmail.com>
2023-07-12 10:32:56 -0300

    inicialize project

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/study-timer'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/study-timer-build/'
+    : '/'
 })
